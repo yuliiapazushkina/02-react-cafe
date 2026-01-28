e65468522b3734de66f38a6cc4af8a86570ec1dd
Julie Pazushkina <162894528+woojulie@users.noreply.github.com>
2026-01-28 15:51:08 +0100

    hw2

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
-import type { Votes, VoteType } from "../types/votes";
+import type { Votes, VoteType } from "../../types/votes";
 
 import css from "./App.module.css";
-import CafeInfo from "./CafeInfo";
-import VoteOptions from "./VoteOptions";
-import VoteStats from "./VoteStats";
-import Notification from "./Notification";
+import CafeInfo from "../CafeInfo/CafeInfo";
+import VoteOptions from "../VoteOptions/VoteOptions";
+import VoteStats from "./VoteStats/VoteStats";
+import Notification from "../Notification/Notification";
 
 const initialVotes: Votes = {
   good: 0,
--- src/components/VoteOptions/VoteOptions.tsx
+++ src/components/VoteOptions/VoteOptions.tsx
@@ -1,4 +1,4 @@
-import type { VoteType } from "../types/votes";
+import type { VoteType } from "../../types/votes";
 import css from "./VoteOptions.module.css";
 
 interface VoteOptionsProps {
