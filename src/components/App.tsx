c6fbb504677da9ec66800272bcceb8a32b2e92ca
Julie Pazushkina <162894528+woojulie@users.noreply.github.com>
2026-01-28 15:06:54 +0100

    hw 2

---
+++ src/components/App.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import type { Votes, VoteType } from "../types/votes";
+
+import css from "./App.module.css";
+import CafeInfo from "./CafeInfo";
+import VoteOptions from "./VoteOptions";
+import VoteStats from "./VoteStats";
+import Notification from "./Notification";
+
+const initialVotes: Votes = {
+  good: 0,
+  neutral: 0,
+  bad: 0,
+};
+
+function App() {
+  const [votes, setVotes] = useState<Votes>(initialVotes);
+
+  const handleVote = (type: VoteType) => {
+    setVotes((prev) => ({
+      ...prev,
+      [type]: prev[type] + 1,
+    }));
+  };
+
+  const resetVotes = () => {
+    setVotes(initialVotes);
+  };
+
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+
+  const positiveRate = totalVotes
+    ? Math.round((votes.good / totalVotes) * 100)
+    : 0;
+
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={totalVotes > 0}
+      />
+
+      {totalVotes > 0 ? (
+        <VoteStats
+          votes={votes}
+          totalVotes={totalVotes}
+          positiveRate={positiveRate}
+        />
+      ) : (
+        <Notification />
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Notification.tsx
@@ -0,0 +1,7 @@
+import css from "./Notification.module.css";
+
+function Notification() {
+  return <p className={css.message}>No feedback yet</p>;
+}
+
+export default Notification;
